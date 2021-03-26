ecbcbc4f9f7805c0bda0a04c60950c865cdc13b8
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-03-26 14:56:16 +0530

    Implemented Theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,9 +2,10 @@
 
 import { Box } from '@material-ui/core';
 import { muiTheme } from 'storybook-addon-material-ui';
+import theme from './theme';
 
 export const decorators = [
-  muiTheme(),
+  muiTheme(theme),
   (Story) => (
     <Box m={2}>
       <Story />
